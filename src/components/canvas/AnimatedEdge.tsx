b1c8d7b20b7194be63d612a4710634ad2f0dfae0
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2025-11-27 16:09:03 +0900

    Create AnimatedEdge.tsx

---
+++ src/components/canvas/AnimatedEdge.tsx
@@ -0,0 +1,48 @@
+/**
+ * @author Lavelle Hatcher Jr
+ * @copyright Copyright (c) 2025 Lavelle Hatcher Jr. All rights reserved.
+ */
+
+import React from 'react';
+import { BaseEdge, getBezierPath, type EdgeProps } from '@xyflow/react';
+import { cn } from '@/lib/utils/cn';
+
+export function AnimatedEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  markerEnd,
+}: EdgeProps) {
+  const [edgePath] = getBezierPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+
+  return (
+    <>
+      <BaseEdge
+        id={id}
+        path={edgePath}
+        markerEnd={markerEnd}
+        style={{
+          ...style,
+          strokeWidth: 2,
+          stroke: '#14b8a6',
+        }}
+        className={cn(
+          'stroke-fresh-teal',
+          '[stroke-dasharray:5] [animation:dataFlow_2s_linear_infinite]'
+        )}
+      />
+    </>
+  );
+}
