b6654e96d143dfb1859aa303ceea63eae243740c
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2025-11-27 15:41:19 +0900

    Create vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+/**
+ * @author Lavelle Hatcher Jr
+ * @copyright Copyright (c) 2025 Lavelle Hatcher Jr. All rights reserved.
+ */
+
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+    include: ['src/**/*.test.{ts,tsx}'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: ['node_modules/', 'src/test/'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
