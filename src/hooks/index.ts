3bf9a84539d50602a5275404e9632fe022337afc
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2025-11-27 17:16:09 +0900

    Create index.ts

---
+++ src/hooks/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @author Lavelle Hatcher Jr
+ * @copyright Copyright (c) 2025 Lavelle Hatcher Jr. All rights reserved.
+ */
+
+export { useKeyboardShortcuts } from './useKeyboardShortcuts';
+export { useExecution } from './useExecution';
+export { useProject } from './useProject';
