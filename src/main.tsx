9e416de6d184461fc62a36e0b5244dd32326eab0
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2025-11-27 15:46:04 +0900

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+/**
+ * @author Lavelle Hatcher Jr
+ * @copyright Copyright (c) 2025 Lavelle Hatcher Jr. All rights reserved.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
