7d14fbe3c13852e798c5dbb25133fd768d5af0b9
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2025-11-27 16:08:22 +0900

    Create index.ts

---
+++ src/components/blocks/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @author Lavelle Hatcher Jr
+ * @copyright Copyright (c) 2025 Lavelle Hatcher Jr. All rights reserved.
+ */
+
+export { BaseBlock } from './BaseBlock';
+export { BlockHeader } from './BlockHeader';
+export { BlockBody } from './BlockBody';
