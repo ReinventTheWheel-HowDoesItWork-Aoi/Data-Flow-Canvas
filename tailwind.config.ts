a9abc4fbf050244b77e073e096b7a33313712f1f
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2025-12-01 08:31:03 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,14 @@ const config: Config = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         'deep-navy': '#1a1a2e',
