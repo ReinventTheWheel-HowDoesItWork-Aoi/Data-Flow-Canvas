a7ed511f14c179fadf177c7e096c1a2703de8b2e
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2025-12-07 05:08:59 +0900

    Update CollaboratorCursors.tsx

--- src/components/collaboration/CollaboratorCursors.tsx
+++ src/components/collaboration/CollaboratorCursors.tsx
@@ -4,7 +4,7 @@
  */
 
 import { memo } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from 'motion/react';
 
 interface CursorData {
   userId: string;
