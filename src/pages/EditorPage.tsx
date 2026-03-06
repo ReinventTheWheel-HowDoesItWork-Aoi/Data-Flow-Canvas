7df8293eb258a1ae1325f47d49b577fef9ee7e99
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2026-03-07 03:17:40 +0900

    Update EditorPage.tsx

--- src/pages/EditorPage.tsx
+++ src/pages/EditorPage.tsx
@@ -14,15 +14,11 @@ import { Canvas } from '@/components/canvas/Canvas';
 import { PyodideLoadingOverlay } from '@/components/PyodideLoadingOverlay';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { TooltipProvider } from '@/components/ui/Tooltip';
-import { Spinner } from '@/components/ui/Spinner';
 import { PyodideProvider } from '@/lib/pyodide';
 import { useUIStore } from '@/stores/uiStore';
-import { useAuthStore } from '@/stores/authStore';
 import { useProject } from '@/hooks/useProject';
 import { useKeyboardShortcuts } from '@/hooks/useKeyboardShortcuts';
 import { cn } from '@/lib/utils/cn';
-import { AuthModal, ProfileCompletionModal } from '@/components/auth';
-import { CollaborationModal } from '@/components/collaboration';
 import { JumpToBlockDialog } from '@/components/canvas/JumpToBlockDialog';
 
 export default function EditorPage() {
@@ -31,7 +27,6 @@ export default function EditorPage() {
   const isDarkMode = useUIStore((state) => state.isDarkMode);
   const setDarkMode = useUIStore((state) => state.setDarkMode);
   const { loadProject, createNewProject, currentProject } = useProject();
-  const { user, isInitialized, needsProfileCompletion } = useAuthStore();
   const initialized = useRef(false);
 
   // Register keyboard shortcuts
@@ -55,18 +50,6 @@ export default function EditorPage() {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [projectId]);
-
-  // Show loading while auth is initializing
-  if (!isInitialized) {
-    return (
-      <div className="min-h-screen bg-bg-primary flex items-center justify-center">
-        <div className="text-center">
-          <Spinner size="lg" />
-          <p className="mt-4 text-text-secondary">Loading...</p>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <ErrorBoundary>
@@ -95,15 +78,6 @@ export default function EditorPage() {
                 <RightPanel />
               </div>
             </div>
-
-            {/* Auth Modal - shows when user is not logged in */}
-            {!user && <AuthModal />}
-
-            {/* Profile Completion Modal - shows when user needs to complete their profile */}
-            {user && needsProfileCompletion && <ProfileCompletionModal />}
-
-            {/* Collaboration Modal - shows when collaborate button is clicked */}
-            <CollaborationModal />
 
             {/* Jump to Block Dialog - shows when Ctrl+G is pressed */}
             <JumpToBlockDialog />
