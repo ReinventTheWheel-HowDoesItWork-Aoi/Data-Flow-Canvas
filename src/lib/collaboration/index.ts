ef227e4cdaaaa5a6cd7c6bcd16066aee1c7f90b8
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2026-03-07 03:18:07 +0900

    Delete src/lib/collaboration directory

--- src/lib/collaboration/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @author Lavelle Hatcher Jr
- * @copyright Copyright (c) 2025 Lavelle Hatcher Jr. All rights reserved.
- */
-
-export { SupabaseCollaborationManager, collaborationManager } from './supabaseRealtime';
