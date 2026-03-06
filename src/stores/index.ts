0619f8fabb4fcc99069eedb8a8b2ddd3d1c35aa2
ReinventTheWheel-HowDoesItWork-Aoi <throwawaytempthrowitup1@proton.me>
2026-03-07 03:24:11 +0900

    Update index.ts

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -3,9 +3,7 @@
  * @copyright Copyright (c) 2025 Lavelle Hatcher Jr. All rights reserved.
  */
 
-export { useAuthStore } from './authStore';
 export { useCanvasStore } from './canvasStore';
-export { useCollaborationStore } from './collaborationStore';
 export { useExecutionStore } from './executionStore';
 export { useProjectStore } from './projectStore';
 export { useUIStore } from './uiStore';
